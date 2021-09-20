7ea6f449bd87e335eae8a3eb6426dc650bbc5a09
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-09-20 08:57:27 +0200

    Add files via upload

---
+++ KlasaProcessZadatakIvanMočilac20092021/KlasaProcessZadatakIvanMočilac20092021/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace KlasaProcessZadatakIvanMočilac20092021
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonIExplorer_Click(object sender, EventArgs e)
+        {
+            Process.Start("IExplore.exe");
+        }
+
+        private void buttonExcel_Click(object sender, EventArgs e)
+        {
+            Process.Start("Excel.exe");
+        }
+
+        private void buttonWord_Click(object sender, EventArgs e)
+        {
+            Process.Start("WinWord.exe");
+        }
+
+        private void buttonStartInfo_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("IExplore.exe");
+
+            startInfo.WindowStyle = ProcessWindowStyle.Normal;
+            startInfo.Arguments = "www.sser.hr";
+        
+            Process.Start(startInfo);
+        }
+    }
+}
